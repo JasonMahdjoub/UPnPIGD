c76668ec7249e6c529bc82841d2d490d2c0b1b14
Jason Mahdjoub <jason.mahdjoub@distri-mind.fr>
2023-04-14 16:17:21 +0200

    Refactor

--- src/main/java/com/distrimind/upnp_igd/transport/Common.java
+++ src/main/java/com/distrimind/upnp_igd/transport/Common.java
@@ -4,12 +4,11 @@ Copyright or © or Corp. Jason Mahdjoub (01/04/2013)
 
 jason.mahdjoub@distri-mind.fr
 
-This software is governed by the CeCILL license under French law and
-abiding by the rules of distribution of free software. The CeCILL license 
-is compatible with GNU GPL license. You can  use, modify and/ or 
-redistribute the software under the terms of the CeCILL
+This software is governed by the CeCILL-C license under French law and
+abiding by the rules of distribution of free software.  You can  use, 
+modify and/ or redistribute the software under the terms of the CeCILL-C
 license as circulated by CEA, CNRS and INRIA at the following URL
-"https://cecill.info/".  
+"http://www.cecill.info". 
 
 As a counterpart to the access to the source code and  rights to copy,
 modify and redistribute granted by the license, users are provided only
@@ -29,8 +28,8 @@ data to be ensured and,  more generally, to use and operate it in the
 same conditions as regards security. 
 
 The fact that you are presently reading this means that you have had
-knowledge of the CeCILL license and that you accept its terms.
- */
+knowledge of the CeCILL-C license and that you accept its terms.
+*/
 
 import com.distrimind.upnp_igd.model.message.IncomingDatagramMessage;
 import com.distrimind.upnp_igd.model.message.header.CallbackHeader;
