7e6f240548c8f672bdfe1609ae80b69741fbede0
Jason Mahdjoub <jason.mahdjoub@distri-mind.fr>
2024-07-03 11:17:27 +0200

    Add notice file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -15,20 +15,20 @@
 
 module com.distrimind.upnp_igd {
 
-	requires jakarta.annotation;
-	requires jakarta.cdi;
-	requires jakarta.inject;
+	requires static jakarta.annotation;
+	requires static jakarta.cdi;
+	requires static jakarta.inject;
 	requires java.datatransfer;
 	requires java.logging;
 	requires java.xml;
 	requires jdk.httpserver;
-	requires jetty.servlet.api;
-	requires org.eclipse.jetty.client;
-	requires org.eclipse.jetty.http;
-	requires org.eclipse.jetty.server;
-	requires org.eclipse.jetty.servlet;
-	requires org.eclipse.jetty.util;
-	requires java.desktop;
+	requires static jetty.servlet.api;
+	requires static org.eclipse.jetty.client;
+	requires static org.eclipse.jetty.http;
+	requires static org.eclipse.jetty.server;
+	requires static org.eclipse.jetty.servlet;
+	requires static org.eclipse.jetty.util;
+	requires static java.desktop;
 	requires static android;
 	requires org.jsoup;
 }
